b55dce5fb7dd5c4e41f02e776c3c37bc8cff01c8
prayas2409 <prayas.hemraj.pagade@gmail.com>
2019-07-13 12:09:51 +0530

    demo taken to try kafka

---
+++ src/main/scala/demo/ProducerApp.scala
@@ -0,0 +1,18 @@
+package demo
+
+import com.knoldus.kafka.producer.{AsyncProducer, KafkaProducer}
+
+
+object ProducerApp extends App {
+
+  val topic = "demo-topic"
+
+  val producer = new AsyncProducer("localhost:9092")
+  //val producer = new KafkaProducer("localhost:9092")
+
+  val batchSize = 100
+  (1 to 1000000).toList.map(no => "Message " + no).grouped(batchSize).foreach { message =>
+    println("Sending message batch size " + message.length)
+    producer.send(topic, message)
+  }
+}
